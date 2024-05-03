f3dc838bb6addf940a6535ec9dedcd2c9bf56866
ankushh19 <ak9108824@gmail.com>
2024-05-03 15:30:37 +0530

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import WeatherApp from "./components/WeatherApp/WeatherApp";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  );
 };
 
 export default App;
